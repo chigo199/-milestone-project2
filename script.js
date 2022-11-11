923a3fb32675114f8d0ee5d4cfe7eabb0079421e
chigo199 <chigo.ukaigwe@gmail.com>
2022-11-11 15:06:09 +0000

    push all files to github

--- script.js
+++ script.js
@@ -1,35 +1,7 @@
-function handleInputKeys (event) {
-    console.log('Received the ' + event.type + " event!");
-    console.log('"this" currently refers to', this.id);
-    console.log('You changed the textbox!\n');
-}
-
-
-function handleBtnClick (event) {
-    console.log('Received the ' + event.type + " event!");
-    console.log('"this" currently refers to', this.id);
-    console.log('You clicked the button!\n');
-  }
-
-  const NavMenu= document.getElementById('show-hide')
-  function showContent() { 
-    
-
-let contentElement = document.querySelector('content')
-if (contentElement.style.display === "none")
-contentElement.style.display = "none";
-  }  
-
-  navMenu.addEventListener('keypress', handleInputKeys); // key presses
-
-
 function openNav() {
-  document.getElementById("mySidenav").style.width = "250px";
-}
-
-function closeNav() {
-  document.getElementById("mySidenav").style.width = "0";
-}
-
-
-   
+    document.getElementById("mySidenav").style.width = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
